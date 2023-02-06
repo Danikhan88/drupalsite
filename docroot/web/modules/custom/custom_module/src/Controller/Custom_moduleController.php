751c21c28c4489be3e9d8e5a082dfdd2c7ced8d7
Danyal zeb <danykhan178@gmail.com>
2023-02-06 10:19:27 +0500

    Version 1

---
+++ docroot/web/modules/custom/custom_module/src/Controller/Custom_moduleController.php
@@ -0,0 +1,11 @@
+<?php
+//  namespace Drupal\custom_module\Controller;
+//  class custom_moduleController
+//  {
+//     public  function  custom_module()
+//     {
+//        $element = array('#markup'=> 'Welcome to Drupal',);
+//        return $element;  
+//     }
+//  } 
+?>
